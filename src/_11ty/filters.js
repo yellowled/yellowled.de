7f41b3fdfa511faac92272957b44a10e6a94f7cb
Matthias Mees <mm@yellowled.de>
2020-07-03 22:12:12 +0200

    Style archive pages, use inline CSS

--- src/_11ty/filters.js
+++ src/_11ty/filters.js
@@ -1,6 +1,11 @@
+const CleanCSS = require('clean-css');
 const { DateTime } = require('luxon');
 
 module.exports = {
+    cssmin: function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    },
+
     dateFormat: function (date, format = 'dd.LL.yyyy') {
         return DateTime.fromJSDate(date).toFormat(String(format));
     },
