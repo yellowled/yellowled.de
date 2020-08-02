4714eb16691c1f623aebbbe97d258c727335ed83
Matthias Mees <mm@yellowled.de>
2020-08-02 18:37:13 +0200

    Remove obsolete cssmin filter

--- src/_11ty/filters.js
+++ src/_11ty/filters.js
@@ -1,11 +1,6 @@
-const CleanCSS = require('clean-css');
 const { DateTime } = require('luxon');
 
 module.exports = {
-    cssmin: function (code) {
-        return new CleanCSS({}).minify(code).styles;
-    },
-
     dateFormat: function (date, format = 'dd.LL.yyyy') {
         return DateTime.fromJSDate(date).toFormat(String(format));
     },
