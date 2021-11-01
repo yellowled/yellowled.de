4b270deda83f94711a23e56cecc342b21bf95d5b
Matthias Mees <mm@yellowled.de>
2021-11-01 10:22:33 +0100

    Upgrade to stylelint-config-standard-scss

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,19 @@
 module.exports = {
-    extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
+    extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
     rules: {
         indentation: 4,
         'no-descending-specificity': null,
+        'alpha-value-notation': [
+            'percentage',
+            {
+                exceptProperties: ['opacity'],
+            },
+        ],
+        'property-no-vendor-prefix': [
+            true,
+            {
+                ignoreProperties: ['appearance', 'text-size-adjust'],
+            },
+        ],
     },
 };
