f65641fa037a62fbb41d5ffd7628b2d50315bbaf
Matthias Mees <mm@yellowled.de>
2022-09-15 12:23:48 +0200

    Remove service worker

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,11 @@ import whenDomReady from 'when-dom-ready';
 
 import { jsReady } from './util/jsReady';
 
-import { initSW } from './modules/serviceWorker';
 import { darkMode } from './modules/darkMode';
 import { initSearch } from './modules/search';
 
 whenDomReady(() => {
     jsReady();
-    initSW();
     darkMode();
     initSearch();
 });
