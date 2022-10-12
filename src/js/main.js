b03b751b596ba62a6acf86b912d1393cefea0246
Matthias Mees <mm@yellowled.de>
2022-10-12 09:47:21 +0200

    Remove legacy JS

--- src/js/main.js
+++ src/js/main.js
@@ -6,15 +6,6 @@ import { darkMode } from './modules/darkMode';
 import { initSearch } from './modules/search';
 
 whenDomReady(() => {
-    if (window.navigator && navigator.serviceWorker) {
-        navigator.serviceWorker
-            .getRegistrations()
-            .then(function (registrations) {
-                for (let registration of registrations) {
-                    registration.unregister();
-                }
-            });
-    }
     jsReady();
     darkMode();
     initSearch();
