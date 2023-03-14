a4595d4871d2e8e038407305c0b1310fa0085884
Matthias Mees <mm@yellowled.de>
2023-03-14 20:55:05 +0100

    Remove no-js utility

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,9 @@
 import whenDomReady from "when-dom-ready";
 
-import { jsReady } from "./util/jsReady";
-
 import { darkMode } from "./modules/darkMode";
 import { initSearch } from "./modules/search";
 
 whenDomReady(() => {
-    jsReady();
     darkMode();
     initSearch();
 });
