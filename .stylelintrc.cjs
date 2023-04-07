3f83a6fe00c602f13601df78423d232f5fbef01a
Matthias Mees <mm@yellowled.de>
2023-04-07 15:29:08 +0200

    Integrate stylelint for .astro, .scss + .svelte files
    Closes #17

---
+++ .stylelintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: ["stylelint-config-standard-scss"],
+    rules: {
+        "no-descending-specificity": null,
+        "alpha-value-notation": [
+            "percentage",
+            {
+                exceptProperties: ["opacity"],
+            },
+        ],
+        "property-no-vendor-prefix": [
+            true,
+            {
+                ignoreProperties: ["appearance", "text-size-adjust"],
+            },
+        ],
+    },
+    customSyntax: "postcss-html",
+};
