4fa7763dc2f30b90b4842d1eb2eeea2890ac4f80
Matthias Mees <mm@yellowled.de>
2023-09-21 20:55:59 +0200

    Simplify theme switching, respect system preference
    Closes #27

--- src/scripts/darkmode.js
+++ src/scripts/darkmode.js
@@ -1,14 +1,16 @@
+const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
 const setColorMode = (mode) => {
-    document.documentElement.setAttribute("data-theme", mode);
-    window.localStorage.setItem("theme", mode);
-    document.querySelector("#theme").checked = mode === "dark";
+    if (mode) {
+        document.documentElement.setAttribute("data-theme", mode);
+        window.localStorage.setItem("theme", mode);
+        document.querySelector("#theme").checked = mode === "dark";
+    }
 };
 
 document.querySelector("#theme").addEventListener("click", (e) => {
     setColorMode(e.target.checked ? "dark" : "light");
 });
-
-const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
 mediaQuery.addEventListener("change", () => {
     if (document.documentElement.getAttribute("data-theme")) {
