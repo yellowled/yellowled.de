d6d571b10b09b374dbc2d5dd7638b8abe4b4f1aa
Matthias Mees <mm@yellowled.de>
2024-10-19 11:34:20 +0200

    Convert cover letter to astro page, export variables from meta data file

--- src/data/meta.js
+++ src/data/meta.js
@@ -1,1 +1,6 @@
+import { differenceInYears } from "date-fns";
+
+export const looking = false;
+export const experience = differenceInYears(new Date(), new Date("2007-11-15"));
+
 export const VERIFICATION = "C5NPRd8kRAwEJZ7GBPoWGOGYxWXLCUc8lnEjDJUjUKQ";
